a919fb20b5737a4d3038af36b1f3e009b5b2d7dd
polluxstarship <sys8364@gmail.com>
2023-04-20 23:33:57 +0900

    first commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
+
         System.out.println("Hello world!");
+        Main_Entry a = new Main_Entry();
+        a.mainentry();
     }
+
 }
---
+++ src/Main_Entry.java
@@ -0,0 +1,5 @@
+public class Main_Entry {
+    public void mainentry() {
+    System.out.println("Hello world!");
+    }
+}
